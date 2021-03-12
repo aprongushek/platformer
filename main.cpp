faa73b936cec61dd4bce4921a084806f3dee77c0
aprongushek <aprongushek@gmail.com>
2021-03-12 12:52:00 +0300

    creating window just for a test

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <conio.h>
+#include <GL/gl.h>
+#include "include/GLFW/glfw3.h"
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    	glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
+int main ()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	int width = 800;
+	int height = 600;
+	GLFWwindow* window = glfwCreateWindow(width, height, "game", nullptr, nullptr);
+	if (window == nullptr) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetKeyCallback(window, key_callback); 
+	  
+	glViewport(0, 0, width, height);
+
+	glClearColor(0.0, 1.0, 0.0, 1.0);
+	
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+	}
+
+	glfwTerminate();
+	return 0;
+}
